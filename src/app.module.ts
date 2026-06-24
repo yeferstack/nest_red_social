22118bbf7e0516944701748d8a89b3d7bcbf9bad
Yefer <yefersonnjar02192510@gmail.com>
2026-06-24 13:57:27 -0500

    se agrega el import de reacciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { RolesModule } from './modules/roles/roles.module';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { PublicacionesModule } from './modules/publicaciones/publicaciones.module';
 import { ComentariosModule } from './modules/comentarios/comentarios.module';
+import { ReaccionesModule } from './modules/reacciones/reacciones.module';
 
 
 
@@ -19,6 +20,7 @@ import { ComentariosModule } from './modules/comentarios/comentarios.module';
     UsuariosModule,
     PublicacionesModule,
     ComentariosModule,
+    ReaccionesModule,
     MongooseModule.forRoot(process.env.MONGO_URI as string),
   ],
   controllers: [AppController],
